2adc06633e9eaa3af93c7fd5f7c85c337d710e64
ap6919 <ap6919@nyu.edu>
2021-11-20 15:41:48 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -47,9 +47,25 @@ def build_packet():
     # Append checksum to the header.
     #Create packet similar to homework 4
     myID = os.getpid() & 0xFFFF 
+    # Header is type (8), code (8), checksum (16), id (16), sequence (16)
+
     myChecksum = 0
+    # Make a dummy header with a 0 checksum
+    # struct -- Interpret strings as packed binary data
     header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, myID, 1)
     data = struct.pack("d", time.time())
+    # Calculate the checksum on the data and the dummy header.
+    myChecksum = checksum(header + data)
+
+    # Get the right checksum, and put in the header
+
+    if sys.platform == 'darwin':
+        # Convert 16-bit integers from host to network  byte order
+        myChecksum = htons(myChecksum) & 0xffff
+    else:
+        myChecksum = htons(myChecksum)
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, myID, 1)
+
     # Don’t send the packet yet , just return the final packet in this function.
     #Fill in end
 
@@ -63,7 +79,7 @@ def get_route(hostname):
     tracelist1 = [] #This is your list to use when iterating through each trace 
     tracelist2 = [] #This is your list to contain all traces
 
-    for ttL in range(1,MAX_HOPS):
+    for ttl in range(1,MAX_HOPS):
         tracelist1 = [] # Reset list for this trace attempt
         for tries in range(TRIES): 
             destAddr = gethostbyname(hostname) #Gets ip address of traceroute query
@@ -72,20 +88,20 @@ def get_route(hostname):
             icmp = getprotobyname("icmp") 
             mySocket = socket(AF_INET, SOCK_RAW, icmp) #create socket that accepts icmp requests
             #Fill in end
-            mySocket.setsockopt(IPPROTO_IP, IP_TTL, struct.pack('I', ttL))
+            mySocket.setsockopt(IPPROTO_IP, IP_TTL, struct.pack('I', ttl))
             mySocket.settimeout(TIMEOUT)
             try:
                 d = build_packet() #builds packet
-                mySocket.sendto(d, (hostname, 0)) #sends packet to fqdn
+                mySocket.sendto(d, (destAddr, 0)) #sends packet to fqdn
                 t=time.time() #timestamps for no reason? might be for sending time but thats in packet
                 startedSelect = time.time() #timestamps again
                 whatReady = select.select([mySocket], [], [], timeLeft) #Creates a timer for socket, will throw timeout error
                 howLongInSelect = (time.time() - startedSelect) #decides how long select has been running
                 if whatReady[0] == []: # Timeout - If nothing is ready
-                    tracelist1.append(str(ttL)) #add which step it is
+                    tracelist1.append(str(ttl)) #add which step it is
                     tracelist1.append("*") # padding
                     tracelist1.append("Request timed out.") #Error
-                    print(f"{ttL} Request Timed Out") 
+                    print(f"{str(ttl)} Request Timed Out") 
                     #Fill in start
                     #You should add the list above to your all traces list
                     tracelist2.append(tracelist1) # Adds Trace to our master list
@@ -94,10 +110,10 @@ def get_route(hostname):
                 timeReceived = time.time() #takes a time stamp of when it as received
                 timeLeft = timeLeft - howLongInSelect #decides if a packet has waited too long.
                 if timeLeft <= 0:
-                    tracelist1.append(str(ttL)) #add which step it is
+                    tracelist1.append(str(ttl)) #add which step it is
                     tracelist1.append("*") # padding
                     tracelist1.append("Request timed out.") #Error
-                    print(f"{ttL} Request Timed Out") 
+                    print(f"{str(ttl)} Request Timed Out") 
                     #Fill in start
                     #You should add the list above to your all traces list
                     tracelist2.append(tracelist1) # Adds Trace to our master list
@@ -108,7 +124,6 @@ def get_route(hostname):
             else: # if there is a response
                 types = struct.unpack("bbHHh", recvPacket[20:28])[0] #Fetch the icmp type from the IP packet
                 sourceIP = addr[0] # _RetAddr form socket
-                ttl = str(ttL) # string to be correct output
                 bytes = struct.calcsize("d") #extrenuous from skeleton code
                 timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0] #extrenuous from skeleton code
                 timeTaken = str(int((timeReceived-t)*1000))+"ms" #based on t(line 80) and timeReceived(line 94)
@@ -126,30 +141,30 @@ def get_route(hostname):
 
                 if types == 11: #ttl Expired
                     #Fill in start TODO
-                    tracelist2.append([ttl,timeTaken,sourceIP,sourceHost]) # append trace to master list
-                    print(f"{ttl} {timeTaken} {sourceIP} {sourceHost}")
+                    tracelist2.append([str(ttl),timeTaken,sourceIP,sourceHost]) # append trace to master list
+                    print(f"{str(ttl)} {timeTaken} {sourceIP} {sourceHost}")
                     #You should add your responses to your lists here
                     #Fill in end
                 elif types == 3: #Host Unreachable
                     #Fill in start TODO
-                    tracelist2.append([ttl,timeTaken,sourceIP,sourceHost]) # append trace to master list
-                    print(f"{ttl} {timeTaken} {sourceIP} {sourceHost}")
+                    tracelist2.append([str(ttl),timeTaken,sourceIP,sourceHost]) # append trace to master list
+                    print(f"{str(ttl)} {timeTaken} {sourceIP} {sourceHost}")
                     #You should add your responses to your lists here 
                     #Fill in end
                 elif types == 0: #Successful ping
                     #Fill in start TODO
-                    tracelist2.append([ttl,timeTaken,sourceIP,sourceHost]) # append trace to master list
+                    tracelist2.append([str(ttl),timeTaken,sourceIP,sourceHost]) # append trace to master list
                     #You should add your responses to your lists here and return your list if your destination IP is met
-                    print(f"{ttl} {timeTaken} {sourceIP} {sourceHost}")
+                    print(f"{str(ttl)} {timeTaken} {sourceIP} {sourceHost}")
                     if sourceIP == destAddr: #if ip from _RetAddr is equal to destAddr
                         print(f"{type(tracelist2)}: {tracelist2}")
                         return tracelist2 #return final list
                     #Fill in end
                 else: #any other
                     #Fill in start TODO
-                    tracelist2.append([ttl,timeTaken,sourceIP,sourceHost])
+                    tracelist2.append([str(ttl),timeTaken,sourceIP,sourceHost])
                     #If there is an exception/error to your if statements, you should append that to your list here
-                    print(f"{ttl} {timeTaken} {sourceIP} {sourceHost}")
+                    print(f"{str(ttl)} {timeTaken} {sourceIP} {sourceHost}")
                     #Fill in end
                 break
             finally:
